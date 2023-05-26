083820ac20656fabc09375d435b3533166b95a7e
s3930426 <95325815+s3930426@users.noreply.github.com>
2023-05-26 11:17:42 +0700

    delete
    delete

--- server/dist/contract_detail/contract_detail.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { BaseEntity } from "typeorm";
-import { Contract } from '../contract/contract.entity';
-export declare class ContractDetail extends BaseEntity {
-    id: string;
-    contract_id: Contract;
-    product_id: string;
-    quantity: number;
-}
