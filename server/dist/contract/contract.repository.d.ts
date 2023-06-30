69df1e6bc9a5d418e2947316f3456068079b70ba
Minh Thong <s3852882@rmit.edu.vn>
2023-06-30 15:12:40 +0700

    refesh main

--- server/dist/contract/contract.repository.d.ts
+++
@@ -1,4 +0,0 @@
-import { Repository } from "typeorm";
-import { Contract } from "./contract.entity";
-export declare class TaskRepository extends Repository<Contract> {
-}
--- server/src/contract/contract.repository.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { EntityRepository, Repository } from "typeorm";
-import { Contract} from "./contract.entity";
-
-
-@EntityRepository(Contract)
-export class TaskRepository extends Repository<Contract> {
-
-}
