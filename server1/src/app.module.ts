b15584c7e6b5b1520ad98da47f72cb150ac9fe70
s3930426 <95325815+s3930426@users.noreply.github.com>
2023-08-01 12:49:32 +0700

    add database connection

--- server1/src/app.module.ts
+++ server1/src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ContractModule } from './contract/contract.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'ubuntu',
+      password: 'password',
+      database: 'capstone_db',
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
+    ContractModule],
   
 })
 export class AppModule {}
