2016887c3a2f62bb356c7069f8c831e103b68c16
Tuong <hoangnhattuong021198@gmail.com>
2023-08-09 14:25:20 +0700

    Split db config using @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import dbConfig from './config/db.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'ec2-18-143-76-192.ap-southeast-1.compute.amazonaws.com',
-    port: 5432,
-    username: 'ubuntu',
-    password: 'password',
-    database: 'dev',
-    entities: [],
-    synchronize: true
-  })],
+  imports:
+    [
+      ConfigModule.forRoot({
+        isGlobal: true,
+        load: [dbConfig],
+        expandVariables: true
+      }),
+      TypeOrmModule.forRootAsync({
+        useFactory: dbConfig
+      })
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
