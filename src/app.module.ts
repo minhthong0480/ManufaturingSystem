efaa7ba85afeed97229f50ea7c24c6905b584698
Tuong <hoangnhattuong021198@gmail.com>
2023-08-09 14:10:59 +0700

    Add typeorm with basic configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'ec2-18-143-76-192.ap-southeast-1.compute.amazonaws.com',
+    port: 5432,
+    username: 'ubuntu',
+    password: 'password',
+    database: 'dev',
+    entities: [],
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
