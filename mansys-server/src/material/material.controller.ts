49530d5de4f95aab054be1c9ef85f82fa4bd3729
Dung Nguyen <dungnov21@gmail.com>
2023-08-10 21:52:54 +0700

    material

--- mansys-server/src/material/material.controller.ts
+++ mansys-server/src/material/material.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Get, Put, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete, Param, Body, Query } from '@nestjs/common';
 import { MaterialService } from './material.service';
 import { CreateMaterialDto } from './dtos/create-material.dto';
 
@@ -12,21 +12,21 @@ export class MaterialController {
     }
 
     @Get()
-        getAllMaterials() {
+        getAllMaterials(@Query('name') name: string) {
         return this.materialService.getAll();
     }
 
-    @Get(':name')
+    @Get('/:name')
         findByMaterialName(@Param('name') name: string) {
         return this.materialService.findByName(name);
     }
 
-    @Put(':id')
+    @Put('/:id')
         updateMaterial(@Param('id') id: number, @Body() updateData: CreateMaterialDto) {
         return this.materialService.update(id, updateData);
     }
 
-    @Delete(':id')
+    @Delete('/:id')
         deleteMaterial(@Param('id') id: number) {
             this.materialService.delete(id);
         return { message: `Material with id ${id} has been deleted` };
--- mansys-server/src/material/material.module.ts
+++ mansys-server/src/material/material.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MaterialService } from './material.service';
 import { MaterialController } from './material.controller';
+import { Material } from './material.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Material])],
   providers: [MaterialService],
   controllers: [MaterialController]
 })
