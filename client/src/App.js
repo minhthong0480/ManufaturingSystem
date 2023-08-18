cff2d7058cc05fc8d487d190ad09d076c38ad781
Nguyễn Quang <s3863937@rmit.edu.vn>
2023-08-18 20:40:26 +0700

    add homepage, header and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Navbar from "./components/Navbar";
 import RegisterPage from "./components/RegisterPage";
 import Dashboard from "./components/Dashboard";
 import CreateContractForm from "./forms/ContractCreateForm";
+import HeaderUI from "./components/HeaderUI";
+import Homepage from "./components/Homepages";
 
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -12,11 +14,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <HeaderUI />
 
       <Routes>
         <Route exact path="/contract" element={<Contact />} />
-        <Route exact path="/" element={<Dashboard />} />
+        <Route exact path="/" element={<Homepage />} />
         <Route exact path="/create_contract" element={<ContractCreate />} />
         <Route exact path="/login" element={<LoginPage />} />
         <Route exact path="/register" element={<RegisterPage />} />
---
+++ client/src/components/Homepages.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import '../Style/Homepages.css'
+import { Card } from 'antd';
+
+import userManagementImg from '../image/user-management.jpg';
+
+const cardData = [
+  {
+    id: 1,
+    imageSrc: {userManagementImg},
+    url: '#',
+    name:"Quản Lý Người Dùng",
+    
+  },
+  {
+    id: 2,
+    imageSrc: '/client-management.jpg',
+    url: '#',
+    name:"Quản Lý Khách Hàng",
+  },
+  {
+    id: 3,
+    imageSrc: {userManagementImg},
+    url: '#',
+    name:"Quản Lý Hợp Đồng",
+  },
+  {
+    id: 4,
+    imageSrc: {userManagementImg},
+    url: '#',
+    name:"Quản Lý Nhân Sự Sản Xuất",
+  },
+  {
+    id: 5,
+    imageSrc: {userManagementImg},
+    url: '#',
+    name:"Quản Lý Vật Tư",
+  },
+  {
+    id: 6,
+    imageSrc: {userManagementImg},
+    url: '#',
+    name:"Quản Lý Cấu Hình",
+  },
+  {
+    id: 7,
+    imageSrc: {userManagementImg},
+    url: '#',
+    name:"Option 7",
+  },
+  {
+    id: 8,
+    imageSrc: {userManagementImg},
+    url: '#',
+    name:"Option 8",
+  },
+];
+
+function Homepage() {
+
+    return (
+        <div className="card-container">
+            <div className="card-row">
+                {cardData.map(card => (
+                    <Card hoverable key={card.id} className="card" >
+                        <img src={card.imageSrc} alt={`From ${card.id}`} />
+                        <a href={card.url}>{card.name}</a>
+                    </Card>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Homepage;
+
