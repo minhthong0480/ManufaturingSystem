ee260deafdd74772a36f9a3521753e453d26e883
mrlittle113 <iboughtmygear@gmail.com>
2023-08-18 14:14:09 +0700

    Add delete contract item by id

--- mansys-server/src/modules/contract/services/contract-item.service.ts
+++ mansys-server/src/modules/contract/services/contract-item.service.ts
@@ -1,36 +1,54 @@
-import { InjectRepository } from "@nestjs/typeorm";
-import { ContractItem } from "../entities/contract-item.entity";
-import { Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
-import { ResultModel } from "src/common/result-model";
-import { CreateContractItemDto } from "../dtos/create-contract-item.dto";
+import { InjectRepository } from '@nestjs/typeorm';
+import { ContractItem } from '../entities/contract-item.entity';
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { ResultModel } from 'src/common/result-model';
+import { CreateContractItemDto } from '../dtos/create-contract-item.dto';
 
 @Injectable()
 export class ContractItemService {
   constructor(
     @InjectRepository(ContractItem)
     private readonly contractItemRepository: Repository<ContractItem>,
-  ) { }
+  ) {}
 
   async deleteByContractId(contractId: number) {
-    const contratItems = await this.contractItemRepository.findBy({ contractId });
+    const contratItems = await this.contractItemRepository.findBy({
+      contractId,
+    });
     await this.contractItemRepository.remove(contratItems);
-    return ResultModel.success(true, "Delete successful!");
+    return ResultModel.success(true, 'Delete successful!');
+  }
+
+  async deleteById(contractId: number, productId: number) {
+    const contratItems = await this.contractItemRepository.findBy({
+      contractId,
+      productId,
+    });
+    await this.contractItemRepository.remove(contratItems);
+    return ResultModel.success(true, 'Delete successful!');
   }
 
   async save(createContractItemDto: CreateContractItemDto) {
     const contractId = createContractItemDto.contractId;
     const productId = createContractItemDto.productId;
 
-    let contractItem = await this.contractItemRepository.findOneBy({ contractId, productId });
+    let contractItem = await this.contractItemRepository.findOneBy({
+      contractId,
+      productId,
+    });
     if (contractItem) {
       contractItem.quanity += createContractItemDto.quantity;
       contractItem = await this.contractItemRepository.save(contractItem);
-    }
-    else {
-      contractItem = await this.contractItemRepository.save(createContractItemDto);
+    } else {
+      contractItem = await this.contractItemRepository.save(
+        createContractItemDto,
+      );
     }
 
-    return ResultModel.success(contractItem, "Create contract-item successful!");
+    return ResultModel.success(
+      contractItem,
+      'Create contract-item successful!',
+    );
   }
 }
