a73bf2344369b938b8728bad02873333afd22e0d
Minh Thong <s3852882@rmit.edu.vn>
2023-08-30 13:58:58 +0700

    change link navbar

--- mansys-client/src/components/HeaderUI.js
+++ mansys-client/src/components/HeaderUI.js
@@ -30,11 +30,11 @@ const HeaderUI = () => {
     const menu = (
       <Menu >
         <Menu.Item key="1"><a href="/">Home</a></Menu.Item>
-        <Menu.Item key="2"><a href="#">Quản Lý Người Dùng</a></Menu.Item>
-        <Menu.Item key="3"><a href="#">Quản Lý Khách Hàng</a></Menu.Item>
+        <Menu.Item key="2"><a href="/employee">Quản Lý Người Dùng</a></Menu.Item>
+        <Menu.Item key="3"><a href="/customer">Quản Lý Khách Hàng</a></Menu.Item>
         <Menu.Item key="4"><a href="/contract">Quản Lý Hợp Đồng</a></Menu.Item>
         <Menu.Item key="5"><a href="#">Quản Lý Nhân Sự Sản Xuất</a></Menu.Item>
-        <Menu.Item key="6"><a href="#">Quản Lý Vật Tư</a></Menu.Item>
+        <Menu.Item key="6"><a href="/material">Quản Lý Vật Tư</a></Menu.Item>
         <Menu.Item key="7"><a href="#">Quản Lý Cấu Hình</a></Menu.Item>
         <Menu.Item key="8"><a href="#">Option 7</a></Menu.Item>
         <Menu.Item key="9"><a href="#">Option 8</a></Menu.Item>
