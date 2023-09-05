6978e45a8227d0f5a3e6b02be62f9e05b22fd5ff
mrlittle113 <iboughtmygear@gmail.com>
2023-09-05 22:14:56 +0700

    Add filter by term + placeholder for edit contract

--- mansys-client/src/services/contract-service.js
+++ mansys-client/src/services/contract-service.js
@@ -1,31 +1,49 @@
-import {AxiosClient} from '../commons/axios-client'
-import {API_CONTRACT_FILTER, API_CONTRACT_CREATE, API_CONTRACT} from '../commons/enum'
-import {requestSucess, requestFail} from '../commons/utilities'
+import { AxiosClient } from '../commons/axios-client'
+import { API_CONTRACT, API_CONTRACT_FILTER, API_CONTRACT_CREATE } from '../commons/enum'
+import { requestSucess, requestFail } from '../commons/utilities'
 
 export const ContractService = {
-    filter : async function(page, pageSize, term, isActive) {
-        const result = await AxiosClient.get(API_CONTRACT_FILTER, {params : {page, pageSize, term, isActive}} )
-        if(result.status >= 400 || !result.data.isSuccess) return requestFail()
-        return requestSucess({
-            data: result.data.data,
-            totalRows : result.data.totalRows
-        })
-    },
 
-    create: async function(data){
-        const result = await AxiosClient.post(API_CONTRACT_CREATE, data)
-        if(result.status >= 400 || !result.data.isSuccess) return requestFail()
-        return requestSucess({
-           data : result.data.data
-        })
-    },
+	get: async function (id) {
+		const api = API_CONTRACT + id;
+		const result = await AxiosClient.get(api);
+		if (result.status >= 400 || !result.data.isSuccess) return requestFail()
+		return requestSucess({
+			data: result.data.data
+		})
+	},
 
-    delete: async function(data) {
-        const api = API_CONTRACT + data.id;
-        const result = await AxiosClient.delete(api)
-        if(result.status >= 400 || !result.data.isSuccess) return requestFail()
-        return requestSucess({
-           data : result.data.data
-        })
-    }
+	filter: async function (page, pageSize, term, isActive) {
+		let result;
+		if (term) {
+			const contractNumber = term;
+
+			result = await AxiosClient.get(API_CONTRACT_FILTER, { params: { page, pageSize, isActive, contractNumber} })
+		}
+		else
+			result = await AxiosClient.get(API_CONTRACT_FILTER, { params: { page, pageSize, isActive } })
+
+		if (result.status >= 400 || !result.data.isSuccess) return requestFail()
+		return requestSucess({
+			data: result.data.data,
+			totalRows: result.data.totalRows
+		})
+	},
+
+	create: async function (data) {
+		const result = await AxiosClient.post(API_CONTRACT_CREATE, data)
+		if (result.status >= 400 || !result.data.isSuccess) return requestFail()
+		return requestSucess({
+			data: result.data.data
+		})
+	},
+
+	delete: async function (data) {
+		const api = API_CONTRACT + data.id;
+		const result = await AxiosClient.delete(api)
+		if (result.status >= 400 || !result.data.isSuccess) return requestFail()
+		return requestSucess({
+			data: result.data.data
+		})
+	}
 }
